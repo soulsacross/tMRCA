86674c89616078c0ef6a9ab879cacc0ba5b5dc59
soulsacross <soulsacross@gmail.com>
2020-08-20 19:03:41 -0300

    Update tMRCA.sh

--- tMRCA.sh
+++ tMRCA.sh
@@ -76,7 +76,7 @@ while read l
 done < $out/$pop.output.txt | 
 
 sed -e 's/  */ /g' | # space to tab substitution not currently working in Mac OSX, should be replaced by \t in Linux
-awk 'NR>2{print $0 } NR%2{p=$7;next}{print($7-p)" "((-25*100*log(0.25))/($7-p)) " " "<<<- tMRCA" }' | # awk -v n=1 '1; NR % n == 0 {print ""}' | #commented cmd would skip evey other line in print statement (I would like than to to this in an entirely new column, which I couldn't get)
+awk '{print $0 } NR%2{p=$7;next}{print($7-p)" "((-25*100*log(0.25))/($7-p)) " " "<<<- tMRCA" }' | # awk -v n=1 '1; NR % n == 0 {print ""}' | #commented cmd would skip evey other line in print statement (I would like than to to this in an entirely new column, which I couldn't get)
 tee $out/GeneticDistances_tMRCA.txt # 
 
 #[ -f $out/output.txt] && rm $out/output.txt
