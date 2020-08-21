35671487dd396e9bee26d3fed7802608a189ce71
soulsacross <soulsacross@gmail.com>
2020-08-21 10:27:20 -0300

    Update tMRCA.sh

--- tMRCA.sh
+++ tMRCA.sh
@@ -29,7 +29,7 @@ echo "==========================================================================
 echo "                         Getting EHH for markers."
 echo "                                                                                   "
 
-#[ -f $out/$pop.output.txt ] && rm $out/$pop.output.txt
+[ -f $out/$pop.output.txt ] && rm $out/$pop.output.txt
 
 # Reading coordinates for selected variants
 {
@@ -69,7 +69,7 @@ echo "                                                                          
 echo "===================================================================================="
 echo "             Getting genetic distances in cM and beginning TMRCA calculation."
 
-# Get genetic distances for such breakpoints of selected haplotypes and calculate tMRCA.
+#Get genetic distances for such breakpoints of selected haplotypes and calculate tMRCA.
 while read l 
     do query_chr=$(echo $l | awk '{print "chr"$1}') 
     query_pos=$(echo $l | awk '{print $2}') 
@@ -77,9 +77,14 @@ while read l
     echo $l " "$cM
 done < $out/$pop.output.txt | 
 
+#cat $out/$pop.output.txt |
 #sed -e 's/  */ /g' | # space to tab substitution not currently working in Mac OSX, should be replaced by \t in Linux
-awk '{print $0} NR%2{ p=$7;next}{if(length($7) != 0){print " "($7-p)" "(-25*100*log(0.25))/($7-p) " " "<<<- tMRCA"}}' | # awk -v n=1 '1; NR % n == 0 {print ""}' | #commented cmd would skip evey other line in print statement (I would like than to to this in an entirely new column, which I couldn't get)
-tee $out/GeneticDistances_tMRCA.txt # 
+awk '{print $0} 
+    NR%2{ p=$7;next}{if(length($7) != 0){
+        print " "($7-p)" "(-25*100*log(0.25))/($7-p) " " "<<<- tMRCA"
+        }
+}' | # awk -v n=1 '1; NR % n == 0 {print ""}' | #commented cmd would skip evey other line in print statement (I would like than to to this in an entirely new column, which I couldn't get)
+tee $out/$pop.GeneticDistances_tMRCA.txt # 
 
 
 echo "                       DONE."
